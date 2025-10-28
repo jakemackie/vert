a822bbeabc55a3aaa8e6a6ef283f2a221c02b920
Jake Mackie <jake@hiyield.co.uk>
2025-10-28 16:11:20 +0000

    fix: mobile layout

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
 <template>
   <!-- Wrapping each section in "section" reads like english, I like doing it -->
   <section class="my-12 container grid-design">
-    <div class="col-span-7 p-4 bg-gray-800/50 rounded-lg">
+    <div class="col-span-full lg:col-span-7 p-4 bg-gray-800/50 rounded-lg">
       Sup!
     </div>
 
-    <div class="col-span-5 flex flex-col gap-4 p-4 bg-gray-800/50 rounded-lg">
+    <div class="col-span-full lg:col-span-5 flex flex-col gap-4 p-4 bg-gray-800/50 rounded-lg">
       You can build awesome layouts really easily with this grid layout
       <div class="aspect-video flex items-center justify-center w-full bg-gray-800 border-2 border-gray-500 rounded-md">
         Imagine there was a video here...
