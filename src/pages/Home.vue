18b1cedcc7080daa57ee4e2c9bd87af777fef5f8
Jake Mackie <jake@hiyield.co.uk>
2025-10-29 10:41:56 +0000

    feat: proof of concept

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="my-12 container grid-design">
-    <div class="col-span-full lg:col-span-7 p-4 bg-gray-800/50 rounded-lg">
-      Sup!
+    <div class="col-span-full lg:col-start-4 lg:col-span-6">
+      <h2 class="text-4xl font-bold text-center">Team Vert</h2>
     </div>
 
-    <div class="col-span-full lg:col-span-5 flex flex-col gap-4 p-4 bg-gray-800/50 rounded-lg">
+    <div class="col-span-full lg:col-start-4 lg:col-span-6 flex flex-col gap-4 p-4 bg-gray-800/50 rounded-lg">
       <a 
         href="/about" 
         class="aspect-video flex items-center justify-center text-center w-full bg-gray-800 border-2 border-gray-500 hover:border-blue-300 rounded-md"
       >
-        Click me to go to the about page
+        <iframe class="size-full" src="https://www.youtube.com/embed/z3o1EYT8s8c?si=FFgPOmQ4ndJW61-s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </a>
     </div>
   </section>
