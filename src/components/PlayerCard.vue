e2942540d10dc4f2a06d9838809684032411ca6b
Jake Mackie <jake@hiyield.co.uk>
2025-11-05 11:39:40 +0000

    feat: improve load times

--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -22,25 +22,25 @@ const profile = computed(() => players[props.player])
     </div>
     <div class="flex justify-between px-4 py-4 sm:px-6">
       <!-- Footer -->
-  <p :class="profile.role === Role.LEAD_TRICKLINER ? 'text-blue-400' : 'text-gray-400'">{{ profile.role }}</p>
+  <p :class="profile.role === Role.LEAD_TRICKLINER ? 'text-blue-400' : 'text-zinc-400'">{{ profile.role }}</p>
       <div class="flex flex-wrap items-center gap-4">
         <a v-if="profile.socials?.tiktok" :href="profile.socials.tiktok" target="_blank" rel="noopener noreferrer" class="hover:rotate-6 hover:scale-125 transition-transform duration-200 ease-out">
           <span class="sr-only">TikTok</span>
-          <TikTokIcon class="size-5 text-gray-400 hover:text-gray-300" />
+          <TikTokIcon class="size-5 text-zinc-400 hover:text-zinc-300" />
         </a>
         <a v-if="profile.socials?.youtube" :href="profile.socials.youtube" target="_blank" rel="noopener noreferrer" class="hover:rotate-6 hover:scale-125 transition-transform duration-200 ease-out">
           <span class="sr-only">YouTube</span>
-          <YouTubeIcon class="size-5 text-gray-400 hover:text-gray-300" />
+          <YouTubeIcon class="size-5 text-zinc-400 hover:text-zinc-300" />
         </a>
         <a v-if="profile.socials?.discord" :href="profile.socials.discord" target="_blank" rel="noopener noreferrer" class="hover:rotate-6 hover:scale-125 transition-transform duration-200 ease-out">
           <span class="sr-only">Discord</span>
-          <DiscordIcon class="size-5 text-gray-400 hover:text-gray-300" />
+          <DiscordIcon class="size-5 text-zinc-400 hover:text-zinc-300" />
         </a>
       </div>
     </div>
 
     <!-- Profile Image -->
-    <div class="absolute -top-1/5 lg:-top-1/4 right-0 size-20 lg:size-24 bg-gray-800 border border-gray-700 drop-shadow-xl drop-shadow-gray-900 rounded-full overflow-hidden">
+    <div class="absolute -top-1/5 lg:-top-1/4 right-0 size-20 lg:size-24 bg-zinc-800 border border-zinc-700 drop-shadow-xl drop-shadow-zinc-900 rounded-full overflow-hidden">
       <img :src="profile.avatar" class="size-full object-contain" />
     </div>
   </article>
